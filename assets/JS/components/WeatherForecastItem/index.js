7de07c5f9c4de4ca8999c301dbff4c472e9c3b8b
Kseniia Kovalenko <kseniiakovalenko.94@gmail.com>
2022-03-30 14:01:05 +0300

    Add third weather option

--- assets/JS/components/WeatherForecastItem/index.js
+++ assets/JS/components/WeatherForecastItem/index.js
@@ -11,11 +11,23 @@ function weatherForecastItem(options = {}) {
 
         return article;
     }
+    // Partially cloudy
+    if(Number(`${options.textTemp}`) < 10 && Number(`${options.textTemp}`) > 5) {
+        const article = `
+        <article class="item">
+            <p>${options.textTime}</p>
+            <img src="/imgs/PartiallyCloudy.png" alt="PartiallyCloudy">
+            <p>${options.textTemp}</p>
+        </article>`;
+
+        return article;
+    }
+
     // Cloudy weather
     const article = `
         <article class="item">
             <p>${options.textTime}</p>
-            <img src="/imgs/cloudy.png" alt="cloudy">
+            <img src="/imgs/Cloudy.png" alt="Cloudy">
             <p>${options.textTemp}</p>
         </article>`;
 
